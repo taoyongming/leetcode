7f8b896c7e25b4742c73f26e2f59bdbfc7121716
tym <oncwnuEMgsPnfGzAJcGGYyPs9l18@git.weixin.qq.com>
2020-07-22 14:21:23 +0800

    提交

---
+++ java/ListNode.java
@@ -0,0 +1,11 @@
+/**
+ * DESCRIPTION :
+ *
+ * @author tym
+ * @ceeate 2020/1/14
+ **/
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+ }
---
+++ java/TreeNode.java
@@ -0,0 +1,12 @@
+/**
+* DESCRIPTION :
+*
+* @author tym
+* @ceeate 2020/1/9
+**/
+public class TreeNode {
+   int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
+}
